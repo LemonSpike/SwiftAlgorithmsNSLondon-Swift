76098e60f085d747d2f57d31045e5b52554ebf78
Pranav Kasetti <pranavkasetti@gmail.com>
2021-04-15 02:27:00 +0100

    Bump Swift Algorithms version.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-algorithms", from: "0.0.4")
+    .package(url: "https://github.com/apple/swift-algorithms", from: "0.1.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a
